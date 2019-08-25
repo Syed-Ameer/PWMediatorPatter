8ccc1b154db396c6af8b2d69a37f10177f2857a6
Syed Ameer <syedameer@live.in>
2019-08-25 21:08:48 +0530

    Base Code Created
    Intial code written for Basic Mediator Pattern

---
+++ BasicMediatorPattern/BasicMediatorPattern/Mediator.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows.Forms;
+
+namespace BasicMediatorPattern
+{
+    public class Mediator
+    {
+        private TextBox _textBox;
+        private ListBox _listBox;
+        private Button _buttonAdd;
+        private Button _buttonClear;
+
+        public void Register(TextBox control)
+        {
+            _textBox = control;
+        }
+        public void Register(ListBox control)
+        {
+            _listBox = control;
+        }
+        public void Register(Button control)
+        {
+            if (control.Name == "btn_Add")
+                _buttonAdd = control;
+
+            if (control.Name == "btn_Clear")
+                _buttonClear = control;
+        }
+        public void TextChange()
+        {
+            bool enableFlag = (_textBox.Text.Length > 0);
+            enableDisableButtons(enableFlag);
+        }
+        public void ClickAddButton()
+        {
+            enableDisableButtons(false);
+
+            _listBox.Items.Add(_textBox.Text);
+            _textBox.ResetText();
+        }
+        public void ClickClearButton()
+        {
+            enableDisableButtons(false);
+            _textBox.ResetText();
+        }
+        private void enableDisableButtons(bool enableFlag = true)
+        {
+            _buttonAdd.Enabled = enableFlag;
+            _buttonClear.Enabled = enableFlag;
+        }
+    }
+}
+
+
+
+
---
+++ BasicMediatorPattern/BasicMediatorPattern/frmCountries.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BasicMediatorPattern
+{
+    public partial class frmCountries : Form
+    {
+        private readonly Mediator _mediator = new Mediator();
+
+        public frmCountries()
+        {
+            InitializeComponent();
+
+            _mediator.Register(txt_Country);
+            _mediator.Register(lst_Countries);
+            _mediator.Register(btn_Add);
+            _mediator.Register(btn_Clear);
+        }
+
+        private void Btn_Add_Click(object sender, EventArgs e)
+        {
+            _mediator.ClickAddButton();
+        }
+
+        private void Btn_Clear_Click(object sender, EventArgs e)
+        {
+            _mediator.ClickClearButton();
+        }
+
+        private void Txt_Country_TextChanged(object sender, EventArgs e)
+        {
+            _mediator.TextChange();
+        }
+    }
+}
